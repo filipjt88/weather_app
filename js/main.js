59632c0920b25fea6ded90f0ccdce7a954b7ab52
filipjt88 <filip88ks@gmail.com>
2025-04-30 22:53:46 +0200

    final app

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const iconCode = data.weather[0].icon;
+const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+function getWeather() {
+    const city = document.getElementById("cityInput").value;
+    const apiKey = "25cd2a113fc23c0b1fcb15fc305c4ee6";
+  
+    if (!city) {
+      alert("Unesite naziv grada.");
+      return;
+    }
+  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Grad nije pronadjen");
+        }
+        return response.json();
+      })
+      .then(data => {
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+      
+        const weatherDiv = document.getElementById("weatherResult");
+        weatherDiv.innerHTML = `
+          <h2>${data.name}, ${data.sys.country}</h2>
+          <img src="${iconUrl}" alt="Weather icon">
+          <p><strong>Temperatura:</strong> ${data.main.temp} °C</p>
+          <p><strong>Vreme:</strong> ${data.weather[0].main}</p>
+          <p><strong>Opis:</strong> ${data.weather[0].description}</p>
+          <p><strong>Vlaznost vazduha:</strong> ${data.main.humidity}%</p>
+        `;
+      })
+      .catch(error => {
+        document.getElementById("weatherResult").innerHTML = `<p style="color:red;">${error.message}</p>`;
+      });
+  }
+  
